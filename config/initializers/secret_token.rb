d95dbcb5b97a8d72e264730e3d98492e886f1f31
Marcelle <mguine@gmail.com>
2011-12-21 08:42:15 -0200

    projeto add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmigoOculto::Application.config.secret_token = 'e763885b7d3da42eaf5e2910e54ef1bd734f66b9b4cf9ad53e27081cd0345845a7ee3362a63cbd3bc512fc65fd0dcb8f3bf39f8ec5e90fc37588b0562fd2ef65'
